bff6145265b74d220c621319fc53148c40b473ff
Gus Vroustouris <gvrousto@purdue.edu>
2020-01-26 23:18:33 -0600

    Updates

---
+++ precache-manifest.0d497ed2e47c97879d2dc6765de16538.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ec7424f81f78276eb084a3fbff5533",
+    "url": "/conveyor-app/index.html"
+  },
+  {
+    "revision": "ceb6d9fa24a59991cc9e",
+    "url": "/conveyor-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "607ececef07d8e0ef7b5",
+    "url": "/conveyor-app/static/css/main.2999cb03.chunk.css"
+  },
+  {
+    "revision": "ceb6d9fa24a59991cc9e",
+    "url": "/conveyor-app/static/js/2.38bc0f95.chunk.js"
+  },
+  {
+    "revision": "a9c42ea4e5335a7c47dd6447ccfe9877",
+    "url": "/conveyor-app/static/js/2.38bc0f95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "607ececef07d8e0ef7b5",
+    "url": "/conveyor-app/static/js/main.77b57b42.chunk.js"
+  },
+  {
+    "revision": "9e9b7010f094bfa9bc73",
+    "url": "/conveyor-app/static/js/runtime-main.1ae8cf98.js"
+  },
+  {
+    "revision": "5a78d8fb714e8296d969e76974784c83",
+    "url": "/conveyor-app/static/media/baseball_gloves.5a78d8fb.png"
+  },
+  {
+    "revision": "d83d2e5842c2b1eb5d3ca38987ea165e",
+    "url": "/conveyor-app/static/media/baseball_gloves_parka.d83d2e58.png"
+  },
+  {
+    "revision": "222a3b5d75944318765dc59db10354fe",
+    "url": "/conveyor-app/static/media/blazer.222a3b5d.png"
+  },
+  {
+    "revision": "6511cb578c927dc4cd38781f21f7dd12",
+    "url": "/conveyor-app/static/media/coaches_jacket.6511cb57.png"
+  },
+  {
+    "revision": "1451f324ddceec7b2ec10beb9f47e23f",
+    "url": "/conveyor-app/static/media/cotton_lace.1451f324.png"
+  },
+  {
+    "revision": "0bd8caa0e1b5d22e1551f71a6a072cfc",
+    "url": "/conveyor-app/static/media/crew_neck_sweatshirt.0bd8caa0.png"
+  },
+  {
+    "revision": "0489e9df71dc6f060fdfe8551add0ea0",
+    "url": "/conveyor-app/static/media/cropped_trousers.0489e9df.png"
+  },
+  {
+    "revision": "5da4e1a30abe66d2fc53e8213dad2f0b",
+    "url": "/conveyor-app/static/media/fabric_samples.5da4e1a3.png"
+  },
+  {
+    "revision": "6658375343f396cfbbbb6bb5892cca3d",
+    "url": "/conveyor-app/static/media/five_pocket_trousers.66583753.png"
+  },
+  {
+    "revision": "1a62324f1a3618386dba047bd4cdcebe",
+    "url": "/conveyor-app/static/media/hooded_sweatshirt.1a62324f.png"
+  },
+  {
+    "revision": "c77d0bf2d3d647a9e7a946aad422290c",
+    "url": "/conveyor-app/static/media/kidd_leather_gloves.c77d0bf2.png"
+  },
+  {
+    "revision": "09d23b9177068b0677ad7dedbc6b53cc",
+    "url": "/conveyor-app/static/media/long_sleeve_henley_shirt.09d23b91.png"
+  },
+  {
+    "revision": "5152c613083bbec3abac211e975f3fe8",
+    "url": "/conveyor-app/static/media/military_surplus_fabrics.5152c613.png"
+  },
+  {
+    "revision": "f47a1331ba47ffc2ecbacd281d59e164",
+    "url": "/conveyor-app/static/media/oxford_shirt.f47a1331.png"
+  },
+  {
+    "revision": "62ed8b954761dd76dc8ca8828ef17f03",
+    "url": "/conveyor-app/static/media/parka.62ed8b95.png"
+  },
+  {
+    "revision": "095cab6758feda424022da0d368e384c",
+    "url": "/conveyor-app/static/media/pile_of_jeans.095cab67.png"
+  },
+  {
+    "revision": "1cfadf38a8c6998f450e2f885aa8c932",
+    "url": "/conveyor-app/static/media/polo_shirt.1cfadf38.png"
+  },
+  {
+    "revision": "a3e9bca435e2105ff02f32222a6052ac",
+    "url": "/conveyor-app/static/media/poncho.a3e9bca4.png"
+  },
+  {
+    "revision": "78012c4b83241642a246842dcd70a7d5",
+    "url": "/conveyor-app/static/media/rugby_shirt.78012c4b.png"
+  },
+  {
+    "revision": "702d425ea3762b130624155fb989d930",
+    "url": "/conveyor-app/static/media/short_pants.702d425e.png"
+  },
+  {
+    "revision": "1b2a131ffad0ca785a2a10611d8b9c09",
+    "url": "/conveyor-app/static/media/short_sleeve_button_up_shirt.1b2a131f.png"
+  },
+  {
+    "revision": "31f837feb57ed8c6cb4caf8579330782",
+    "url": "/conveyor-app/static/media/t-shirt.31f837fe.png"
+  },
+  {
+    "revision": "8e9c3d1e1e42922e75950b69faa9ab2e",
+    "url": "/conveyor-app/static/media/tote_bag.8e9c3d1e.png"
+  },
+  {
+    "revision": "c7216bda9237ce1f5943f61fca1e1131",
+    "url": "/conveyor-app/static/media/tracksuit_jacket.c7216bda.png"
+  },
+  {
+    "revision": "83669219f206e93cf667c7d6e70ec4f8",
+    "url": "/conveyor-app/static/media/trenchcoat.83669219.png"
+  }
+]);
