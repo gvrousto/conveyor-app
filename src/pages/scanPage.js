532bb09329c5cfc67d35c03e232af61f6fdd3075
Gus Vroustouris <gvrousto@purdue.edu>
2020-04-11 19:11:15 -0500

    scan page is done except for exact sizing and styling

--- src/pages/scanPage.js
+++ src/pages/scanPage.js
@@ -3,42 +3,48 @@ import {Line} from 'react-shapes';
 import Webcam from "react-webcam";
 import {Button} from 'react-bootstrap';
 import ReactPlayer from 'react-player';
+import Sound from 'react-sound';
 import './style/scanPage.css'
 
 
-class SelectPage extends React.Component{
+class ScanPage extends React.Component{
   render(){
     return(
     <div className="d-flex flex-column justify-content-center webcam-container">
-      <div className="d-flex justify-content-center webcam-header">Input Materials</div>
+      <Sound/>
+      <div className="d-flex justify-content-center webcam-header">
+        Input Materials
+      </div>
       <div className="d-flex justify-content-center">
-        <ReactPlayer height={550} className="d-flex webcam-header" url='https://www.youtube.com/watch?v=ysz5S6PUM-U' playing />
-        <div className="stacked-div shapes-container">
-          <div className="stacked-div">
-            <Line x1={90} x2={200} y1={150} y2={150} stroke={{color:'#E65243'}} strokeWidth={20} />
+        <div className="d-flex justify-content-center align-items-center shapes-container">
+          <ReactPlayer height={650} className="d-flex webcam-header" url='https://youtu.be/Xlasn8_mrwQ?start=207&end=208' playing />
+          <div className="stacked-div lines-container">
+            <div className="stacked-div">
+              <Line x1={0} x2={110} y1={10} y2={10} stroke={{color:'#E65243'}} strokeWidth={20} />
+            </div>
+            <div className="stacked-div">
+              <Line x1={10} x2={10} y1={0} y2={100} stroke={{color:'#E65243'}} strokeWidth={20} />
+            </div>
+            <div className="stacked-div">
+              <Line x1={260} x2={370} y1={10} y2={10} stroke={{color:'#E65243'}} strokeWidth={20} />
+            </div>
+            <div className="stacked-div">
+              <Line x1={370} x2={370} y1={0} y2={100} stroke={{color:'#E65243'}} strokeWidth={20} />
+            </div>
+            <div className="stacked-div">
+              <Line x1={0} x2={110} y1={240} y2={240} stroke={{color:'#E65243'}} strokeWidth={20} />
+            </div>
+            <div className="stacked-div">
+              <Line x1={10} x2={10} y1={140} y2={240} stroke={{color:'#E65243'}} strokeWidth={20} />
+            </div>
+            <div className="stacked-div">
+              <Line x1={260} x2={370} y1={240} y2={240} stroke={{color:'#E65243'}} strokeWidth={20} />
+            </div>
+            <div className="stacked-div">
+              <Line x1={370} x2={370} y1={150} y2={250} stroke={{color:'#E65243'}} strokeWidth={20} />
+            </div>
           </div>
           <div className="stacked-div">
-            <Line x1={100} x2={100} y1={150} y2={250} stroke={{color:'#E65243'}} strokeWidth={20} />
-          </div>
-          <div className="stacked-div">
-            <Line x1={365} x2={461} y1={150} y2={150} stroke={{color:'#E65243'}} strokeWidth={20} />
-          </div>
-          <div className="stacked-div">
-            <Line x1={450} x2={450} y1={150} y2={250} stroke={{color:'#E65243'}} strokeWidth={20} />
-          </div>
-          <div className="stacked-div">
-            <Line x1={90} x2={200} y1={400} y2={400} stroke={{color:'#E65243'}} strokeWidth={20} />
-          </div>
-          <div className="stacked-div">
-            <Line x1={100} x2={100} y1={300} y2={400} stroke={{color:'#E65243'}} strokeWidth={20} />
-          </div>
-          <div className="stacked-div">
-            <Line x1={350} x2={461} y1={400} y2={400} stroke={{color:'#E65243'}} strokeWidth={20} />
-          </div>
-          <div className="stacked-div">
-            <Line x1={450} x2={450} y1={300} y2={400} stroke={{color:'#E65243'}} strokeWidth={20} />
-          </div>
-          <div className="d-flex stacked-div shapes-container justify-content-center align-items-center">
             <Button className="button-text" href="#/select/Gloves" variant="danger" size="lg">
               Visit Collection
             </Button>
@@ -50,4 +56,4 @@ class SelectPage extends React.Component{
   }
 }
 
-export default SelectPage;
+export default ScanPage;
