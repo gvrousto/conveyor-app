f5445d40c3235697cd234558d0cfa7c23931c412
Gus Vroustouris <gvrousto@purdue.edu>
2020-04-16 17:56:35 -0500

    fuck comments

--- src/pages/loadPage.js
+++ src/pages/loadPage.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 import './style/loadPage.css'
 
-
-
-
-
-
-
 class LoadPage extends React.Component{
 
   constructor(props){
@@ -30,32 +24,6 @@ class LoadPage extends React.Component{
     let vector = this.props.match.params.vector;
     //call here
   }
-
-  // OLD CODE FROM componentDidMount
-  // setInterval( () => {
-  //   if (this.state.progress<=100){
-  //     if (this.state.progress>10) {
-  //       console.log(this.state.progress);
-  //     }
-  //     let newState = {
-  //       progress: this.state.progress + 100
-  //     }
-  //     this.setState(newState);
-  //   }
-  //   else {
-  //   }
-  // },600);
-
-  // OLD CODE FROM MOVIE FOR LOADING BAR
-  // <div className="progressbar-container">
-  // <div id="bar_container">
-  //   <div style={{width: this.state.progress + '%'}} id="progress_bar">
-  //   <div id="progress_percentage">
-  //     {this.state.progress}%
-  //   </div>
-  //   </div>
-  //   </div>
-  // </div>
 
   render(){
     console.log(this.props.match.params);
