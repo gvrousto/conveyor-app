c26f419f299f0d5134648b42b89fec63ce31690a
Gus Vroustouris <gvrousto@purdue.edu>
2020-04-18 16:32:41 -0500

    scan page now fullymobile friendly

--- src/pages/scanPage.js
+++ src/pages/scanPage.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import {Line} from 'react-shapes';
 import { Button } from 'react-bootstrap';
+import { useMediaQuery } from 'react-responsive';
 import ReactPlayer from 'react-player';
 import {Link} from 'react-router-dom';
 import './style/scanPage.css'
 
-
 class ScanPage extends React.Component{
+
   render(){
-    //const isMobile = useMediaQuery({ query: '(max-width: 425px)' });
-    // let height = "800";
-    // let width = "600";
-    // if(isMobile){
-    //   height = "200";
-    //   width = "100";
-    // }
     return(
       <div className="scan-page-container">
         <div className="scan-header">
@@ -22,52 +16,45 @@ class ScanPage extends React.Component{
         </div>
         <div className="scan-container">
           <div className="shapes-container">
-            <ReactPlayer  className="video-container" url='https://youtu.be/Xlasn8_mrwQ?start=207&end=241' playing />
+            <ReactPlayer height="100%" width="100%" url='https://youtu.be/Xlasn8_mrwQ?start=207&end=241' playing />
+            <div className="stacked-div lines-container">
+              <div className="stacked-div">
+                <Line x1={0} x2={110} y1={10} y2={10} stroke={{color:'#E65243'}} strokeWidth={20} />
+              </div>
+              <div className="stacked-div">
+                <Line x1={10} x2={10} y1={0} y2={100} stroke={{color:'#E65243'}} strokeWidth={20} />
+              </div>
+              <div className="stacked-div">
+                <Line x1={210} x2={310} y1={10} y2={10} stroke={{color:'#E65243'}} strokeWidth={20} />
+              </div>
+              <div className="stacked-div">
+                <Line x1={310} x2={310} y1={0} y2={100} stroke={{color:'#E65243'}} strokeWidth={20} />
+              </div>
+              <div className="stacked-div">
+                <Line x1={0} x2={110} y1={230} y2={230} stroke={{color:'#E65243'}} strokeWidth={20} />
+              </div>
+              <div className="stacked-div">
+                <Line x1={10} x2={10} y1={140} y2={230} stroke={{color:'#E65243'}} strokeWidth={20} />
+              </div>
+              <div className="stacked-div">
+                <Line x1={210} x2={310} y1={230} y2={230} stroke={{color:'#E65243'}} strokeWidth={20} />
+              </div>
+              <div className="stacked-div">
+                <Line x1={310} x2={310} y1={150} y2={240} stroke={{color:'#E65243'}} strokeWidth={20} />
+              </div>
+            </div>
+            <div className="stacked-div">
+              <Link to="/material">
+                <Button className="button-text" variant="danger" size="lg">
+                  Visit Collection
+                </Button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
     );
   }
 }
-
-// <div className="d-flex flex-column justify-content-center webcam-container">
-//   <div className="d-flex justify-content-center webcam-header">
-//     Input Materials
-//   </div>
-//   <div className="d-flex justify-content-center">
-//     <div className="d-flex justify-content-center align-items-center shapes-container">
-//       <ReactPlayer height={650} url='https://youtu.be/Xlasn8_mrwQ?start=207&end=241' playing />
-//       <div className="stacked-div lines-container">
-//         <div className="stacked-div">
-//           <Line x1={0} x2={110} y1={10} y2={10} stroke={{color:'#E65243'}} strokeWidth={20} />
-//         </div>
-//         <div className="stacked-div">
-//           <Line x1={10} x2={10} y1={0} y2={100} stroke={{color:'#E65243'}} strokeWidth={20} />
-//         </div>
-//         <div className="stacked-div">
-//           <Line x1={260} x2={360} y1={10} y2={10} stroke={{color:'#E65243'}} strokeWidth={20} />
-//         </div>
-//         <div className="stacked-div">
-//           <Line x1={360} x2={360} y1={0} y2={100} stroke={{color:'#E65243'}} strokeWidth={20} />
-//         </div>
-//         <div className="stacked-div">
-//           <Line x1={0} x2={110} y1={240} y2={240} stroke={{color:'#E65243'}} strokeWidth={20} />
-//         </div>
-//         <div className="stacked-div">
-//           <Line x1={10} x2={10} y1={140} y2={240} stroke={{color:'#E65243'}} strokeWidth={20} />
-//         </div>
-//         <div className="stacked-div">
-//           <Line x1={260} x2={360} y1={240} y2={240} stroke={{color:'#E65243'}} strokeWidth={20} />
-//         </div>
-//         <div className="stacked-div">
-//           <Line x1={360} x2={360} y1={150} y2={250} stroke={{color:'#E65243'}} strokeWidth={20} />
-//         </div>
-//       </div>
-//       <div className="stacked-div">
-//       <Link to="/material">
-//         <Button className="button-text" variant="danger" size="lg">
-//           Visit Collection
-//         </Button>
-//       </Link>
 
 export default ScanPage;
